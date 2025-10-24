623c99d396b2bc855708a8809f33e4a8dfb8e47a
Artem <aeposokhov@gmail.com>
2025-10-24 10:36:36 -0400

    added vi editing

--- typewriter.py
+++ typewriter.py
@@ -3,8 +3,8 @@ from prompt_toolkit import prompt
 VENDOR_ID = 0x0FE6
 PRODUCT_ID = 0x811E
 p = Usb(VENDOR_ID, PRODUCT_ID)
-text = prompt("\nstart typing ... \n>", multiline=True)
-i = int(input("how much to print:) "))
+text = prompt("\nstart typing ... \n>", multiline=True, vi_mode=True)
+i = int(prompt("how much to print:) "))
 for _ in range(i):
     p.text(text)
     p.cut()
